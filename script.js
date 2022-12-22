cf984e5d2abc2175238f60698b09095ca4de1d48
nko <solowingrazgriz@outlook.com>
2022-12-22 14:33:37 +0900

    Started on contact page

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 //currently unfinished because i'm not sure entirely how I want to do this.
+//what should happen when a user presses the modeButton?
+//swap entire stylesheet?
+//change specific element styles?
 let modeButton = document.getElementById("dark-mode");
-let docText = document.querySelectorAll("*");
+const docText = Array.from(document.getElementsByClassName("doc-text"));
 
 function darkMode()
 {
     document.body.style.backgroundColor = "#252729";
+    for (i=0; i<docText.length; i++)
+    {
+        document.docText[i].style.color = "white";
+    }
 }
 
 modeButton.addEventListener("click", darkMode);
