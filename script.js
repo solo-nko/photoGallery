eaca0b180d3303b9ef63267e23037a751564c5fd
nko <solowingrazgriz@outlook.com>
2023-02-04 12:23:20 +0900

    Add basic light/dark mode functionality to main page

--- script.js
+++ script.js
@@ -2,16 +2,29 @@
 //what should happen when a user presses the modeButton?
 //swap entire stylesheet?
 //change specific element styles?
-let modeButton = document.getElementById("dark-mode");
+let modeDarkButton = document.getElementById("dark-mode");
+let modeLightButton = document.getElementById("light-mode");
 const docText = Array.from(document.getElementsByClassName("doc-text"));
 
 function darkMode()
 {
     document.body.style.backgroundColor = "#252729";
-    for (i=0; i<docText.length; i++)
-    {
-        document.docText[i].style.color = "white";
-    }
+    docText.forEach(element => {
+        element.style.color = "white";
+    })
+    modeDarkButton.style.display = "none";
+    modeLightButton.style.display = "inline-block";
 }
 
-modeButton.addEventListener("click", darkMode);
+function lightMode()
+{
+    document.body.style.backgroundColor = "beige"
+    docText.forEach(element => {
+        element.style.color = "black";
+    });
+    modeDarkButton.style.display = "inline-block";
+    modeLightButton.style.display = "none";
+}
+
+modeDarkButton.addEventListener("click", darkMode);
+modeLightButton.addEventListener("click", lightMode);
