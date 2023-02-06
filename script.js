ddf219314b6cdbb6d628897e602d09dfc90122ab
nko <solowingrazgriz@outlook.com>
2023-02-06 21:20:50 +0900

    Improve dark mode

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-//currently unfinished because i'm not sure entirely how I want to do this.
-//what should happen when a user presses the modeButton?
-//swap entire stylesheet?
-//change specific element styles?
-let modeDarkButton = document.getElementById("dark-mode");
-let modeLightButton = document.getElementById("light-mode");
+const modeDarkButton = document.getElementById("dark-mode");
+const modeLightButton = document.getElementById("light-mode");
+const headerFooter = Array.from(document.getElementsByClassName("header-footer-theme"));
 const docText = Array.from(document.getElementsByClassName("doc-text"));
+
 //light mode palette
 const bgColorLight = "#F5F5DC";
 const textColorLight = "#001D46";
+const accentColorLight = "#7BBAF5"
 
 //dark mode palette
 const bgColorDark = "#001D46";
 const textColorDark = "#F5F5DC";
+const accentColorDark = "#4169E1";
 
 function darkMode()
 {
     document.body.style.backgroundColor = bgColorDark;
     docText.forEach(element => {
-        element.style.color = textColorDark;
-    })
+        element.style.color = textColorDark});
+    headerFooter.forEach(element => {
+        element.style.backgroundColor = accentColorDark;
+    });
     modeDarkButton.style.display = "none";
     modeLightButton.style.display = "inline-block";
 }
@@ -29,6 +31,9 @@ function lightMode()
     docText.forEach(element => {
         element.style.color = textColorLight;
     });
+    headerFooter.forEach(element => {
+        element.style.backgroundColor = accentColorLight;
+    })
     modeDarkButton.style.display = "inline-block";
     modeLightButton.style.display = "none";
 }
